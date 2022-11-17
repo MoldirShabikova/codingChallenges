1740838973df703690956b34fa21a85825996557
moldir <moldirshb@gmail.com>
2022-11-17 10:23:08 -0600

    second solution

--- String Decode/index.js
+++ String Decode/index.js
@@ -16,3 +16,23 @@ console.log(decoder('3vdfn'));
 console.log(decoder('0r'));
 console.log(decoder('2bna0p1mp2osl0e'));
 console.log(decoder('0y4akjfe0s'));
+
+
+const decoder2 = code => {
+    let splitStr = code.split('')
+    let result = ''
+  
+    for (let i = 0; i < splitStr.length; i++) {
+      if (!isNaN(+splitStr[i])) {
+        i += +splitStr[i]
+      } else {
+        result += splitStr[i]
+      }
+    }
+  
+    return result
+  }
+  
+  // decoder2('0y4akjfe0s')
+  // decoder2('0h2xce5ngbrdy')
+  // decoder2('2bna0p1mp2osl0e')
